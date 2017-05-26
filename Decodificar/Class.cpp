773837250241d4a94b3b8c327ea94cc1774a37f6
caionakai <kaioesperto@hotmail.com>
2017-05-26 14:44:48 -0300

    n

---
+++ Decodificar/Class.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   Class.cpp
+ * Author: kaioe
+ * 
+ * Created on 26 de Maio de 2017, 14:40
+ */
+
+#include "Class.h"
+
+Class::Class() {
+}
+
+Class::Class(const Class& orig) {
+}
+
+Class::~Class() {
+}
+
---
+++ Decodificar/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: kaioe
+ *
+ * Created on 26 de Maio de 2017, 14:40
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
